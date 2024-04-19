d9882b3b9b36ab53607a08339f7c3931cb216069
iotimur <timvork03.gmail.com>
2024-04-20 01:01:26 +0300

    Добавил index.ts для main

---
+++ assets/Images_main/index.ts
@@ -0,0 +1,12 @@
+export { default as img_top_1 } from "./The_Witcher_3-_Wild_Hunt_Cover.jpg";
+export { default as img_top_2 } from "./Red_Dead_Redemption_2_coverart.jpg";
+export { default as img_top_3 } from "./Forza.jpg";
+export { default as img_top_4 } from "./atomic.jpg";
+export { default as img_categories_1 } from "./гонки.jpg";
+export { default as img_categories_2 } from "./глубокий сюжет.jpg";
+export { default as img_categories_3 } from "./симуляторы.jpg";
+export { default as img_categories_4 } from "./игры с открытым миром.jpg";
+export { default as img_news_1 } from "./новость1.jpg";
+export { default as img_news_2 } from "./новость2.jpg";
+export { default as arrow_left } from "./arrow_left.png";
+export { default as arrow_right } from "./arrow_right.png";
