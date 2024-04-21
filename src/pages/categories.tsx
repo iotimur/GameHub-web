a232ae1ddd84b5292eedcbbbecb7a7c0027f2684
aaeii <ahm-emily@yandex.ru>
2024-04-21 23:37:57 +0300

    разделение categories на компоненты

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,175 +1,50 @@
 import React from "react";
 import '../css/reset.css';
-import '../css/categories.css';
-import '../css/header/menu.css';
-import '../css/footer.css';
-import { switch_, windows, leaders_game1, leaders_game2, leaders_game3, new_game1, new_game2, new_game3, sales_game1, sales_game2 } from '../../assets/images'
+// import '../css/categories.css';
+// import '../css/header/menu.css';
+import '../css/header_footer1.css';
+import Header from "../components/header/header.jsx";
+import TypesOfCategories from "../components/categories/types-of-categories";
+import { f1, f2, f3, f4, cmc } from '../../assets/images'
 
 const Categories = () => {
     return (
         <>
-            <body>
-                <header>
-                    <div className="outer">
-                        <nav>
-                            <label className="logo">GameHub</label>
-                            <ul className="menu_items">
-                                <li><a href="categories.html" className="active">Категории</a></li>
-                                <li><a href="#">Войти</a></li>
-                                <li><a href="shopping_cart.html">Корзина</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-
-                </header>
-                <main>
-                    <div>
-                        <div className="big-separator"></div>
-                        <div className="big-separator"></div>
-                        <span className="sort-txt">Сортировать по</span>
-                        <button className="filter">
-                            <img className="filter-img" src={switch_} alt="стрелка вниз" width="34" height="21" />
-                        </button>
-
-                        <div className="rounded-rectangle">
-                            <h1 className=" intro-title">
-                                Лидеры продаж
-                            </h1>
-                        </div>
-                        <div className="big-separator"></div>
-
-                        <button className="card ">
-                            <img className="card-img" src={leaders_game1} alt="Обложка игры Counter-Strike 2" />
-                            <span className="title-game">Counter-Strike 2</span>
-                            <img className="img-windows" src={windows} width="40" />
-                            <span className="price-tx price-txt">Бесплатно</span>
-                        </button>
-
-                        <div className="separator"></div>
-                        <button className="card">
-                            <img className="card-img" src={leaders_game2} alt="Обложка игры Palworld" />
-                            <span className="title-game">Palworld</span>
-                            <img className="img-windows" src={windows} width="40" />
-                            <span className="new-price">990 руб.</span>
-                            <span className="old-price">1100 руб.</span>
-                        </button>
-
-                        <div className="separator"></div>
-                        <button className="card group-tr-game-info">
-                            <img className="card-img" src={leaders_game3} alt="Обложка игры PUBG: BATTLEGROUNDS" />
-                            <span className="title-game">PUBG: BATTLEGROUNDS</span>
-                            <img className="img-windows" src={windows} width="40" />
-                            <span className="price-tx price-txt">Бесплатно</span>
-                        </button>
-                        <div className="separator"></div>
-                        <button className="show-more-btn">
-                            <span>Показать больше</span>
-                        </button>
-                        <div className="big-separator"></div>
-                        <div className="big-separator"></div>
-                        <div className="rounded-rectangle">
-                            <span className="intro-title">
-                                Новинки
-                            </span>
-                        </div>
-                        <div className="big-separator"></div>
-                        <button className="card">
-                            <img className="card-img" src={new_game1} alt="Обложка игры Alpha League" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">Alpha League</span>
-                            <span className="price-tx price-txt">Бесплатно</span>
-                        </button>
-                        <div className="separator"></div>
-
-                        <button className="card">
-                            <img className="card-img" src={new_game2}
-                                alt="Обложка игры Sons Of The Forests" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">Sons Of The Forests</span>
-                            <span className="old-price">1100 руб.</span>
-                            <span className="new-price">820 руб.</span>
-                        </button>
-                        <div className="separator"></div>
-
-                        <button className="card">
-                            <img className="card-img" src={new_game3} alt="Обложка игры Pacific Drives" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">Pacific Drives</span>
-                            <span className="new-price">1799 руб.</span>
-                        </button>
-                        <div className="separator"></div>
-                        <button className="show-more-btn">
-                            <span>Показать больше</span>
-                        </button>
-                        <div className="big-separator"></div>
-                        <div className="big-separator"></div>
-                        <div className="rounded-rectangle">
-                            <h1 className="intro-title">Скидки</h1>
-                        </div>
-                        <div className="big-separator"></div>
-                        <button className="card">
-                            <img className="card-img" src={sales_game1} alt="Обложка игры How to Survive" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">How to Survive</span>
-                            <span className="new-price">83 руб.</span>
-                        </button>
-                        <div className="separator"></div>
-
-                        <button className="card">
-                            <img className="card-img" src={sales_game2}
-                                alt="Обложка игры Red Solstice 2 Survivors" width="356" height="166" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">Red Solstice 2 Survivors</span>
-                            <span className="new-price">561 руб.</span>
-                        </button>
-                        <div className="separator"></div>
-                        <button className="card">
-                            <img className="card-img" src={new_game2} alt="Обложка игры Sons Of The Forests" />
-                            <img className="img-windows" src={windows} width="40" height="40" />
-                            <span className="title-game">Sons Of The Forests</span>
-                            <span className="old-price">1100 руб.</span>
-                            <span className="new-price">820 руб.</span>
-                        </button>
-                        <div className="separator"></div>
-                        <button className="show-more-btn">
-                            <span>Показать больше</span>
-                        </button>
-                        <div className="big-separator"></div><div className="big-separator"></div>
-                    </div>
-                </main>
-            </body>
-
+            <Header />
+            <main>
+                <TypesOfCategories />
+            </main>
             <footer className="footer">
                 <div className="d-flex in_footer1">
                     <figure>
-                        <img src="..\assets\images/f1.png" alt="Лучшая цена" />
+                        <img src={f1} alt="Лучшая цена" />
                         <figcaption>Лучшая цена</figcaption>
                     </figure>
                     <figure>
-                        <img src="..\assets\images/f2.png" alt="Гарантия 100%" />
+                        <img src={f2} alt="Гарантия 100%" />
                         <figcaption>Гарантия 100%</figcaption>
                     </figure>
                     <figure>
-                        <img src="..\assets\images/f3.png" alt="Все способы оплаты" />
+                        <img src={f3} alt="Все способы оплаты" />
                         <figcaption>Все способы оплаты</figcaption>
                     </figure>
                     <figure>
-                        <img src="..\assets\images/f4.png" alt="Регулярные акции и скидки" />
+                        <img src={f4} alt="Регулярные акции и скидки" />
                         <figcaption>Регулярные акции и скидки</figcaption>
                     </figure>
                 </div>
-                <hr></hr>
+                <hr />
                 <div className="in_footer d-flex" >
                     <div className="left">
                         <p className="main">Наши контакты:</p>
                         <nav>
                             <p>Техническая поддержка по общим вопросам:</p>
-                            <img src="..\assets\images/cmc.jpg" alt="cmc" />
+                            <img src={cmc} alt="cmc" />
                             <a href="gamehub@gmail.com">gamehub@gmail.com</a>
                         </nav>
                         <nav>
                             <p>Администрация:</p>
-                            <img src="..\assets\/images/cmc.jpg" alt="cmc" />
+                            <img src={cmc} alt="cmc" />
                             <a href="gamehub_admin@gmail.com">gamehub_admin@gmail.com</a>
                         </nav>
                     </div>
@@ -181,6 +56,7 @@ const Categories = () => {
                 <div className="bott">
                     <p className="end">Казань, 2024</p>
                 </div>
+
             </footer>
         </>
     );
