f5b50c7f7f5b72c84276a5707dcbf90fc91c8797
iotimur <timvork03.gmail.com>
2024-04-26 23:31:55 +0300

    Добавил index.ts в img

---
+++ assets/Images_main/favicon_io/index.ts
@@ -0,0 +1,3 @@
+export { default as apple_touch_icon } from "./apple-touch-icon.png";
+export { default as favicon_32x32 } from "./favicon-32x32.png";
+export { default as favicon_16x16 } from "./favicon-16x16.png";
