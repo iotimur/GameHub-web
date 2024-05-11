cd2bcd2843218ddcadffaf747b16951f46f0b959
aaeii <ahm-emily@yandex.ru>
2024-05-11 21:37:16 +0300

    fix path

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import '../css/reset.css';
-import Header from "../components/header/header.jsx";
+import Header from "../components/main/header/header";
 import { Footer } from '../components/footer/footer';
 import MainCategories from "../components/categories/main-categories/main-categories";
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import MainPage from "./pages/main-page";
 import Game_page from "./pages/game_page";
 import Personal_account from "./pages/personal-account";
 import Shopping_cart from "./pages/shopping_cart";
+import Entrance from "./pages/entrance";
 
 export const PageRoutes = () => (
     <Routes>
@@ -13,6 +14,7 @@ export const PageRoutes = () => (
         <Route path="/gamehub/categories" element={<Categories/>}/>
         <Route path="/gamehub/game-page" element={<Game_page/>}/>
         <Route path="/gamehub/personal-account" element={<Personal_account/>}/>
+        <Route path="/gamehub/entrance" element={<Entrance/>}/>
         <Route path="/gamehub/shopping-cart" element={<Shopping_cart/>}/>
 
         <Route path="*" element={<h1>Page not found</h1>}/>
