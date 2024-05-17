f1f4bf8dead0c74d2a95e14fe4f776604257f532
unknown <nastya.mayorova9@icloud.com>
2024-05-17 13:50:19 +0300

    поправки

--- src/pages/shopping_cart.tsx
+++ src/pages/shopping_cart.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 
-import '../css/style_shopping_cart.css';
-import Games from '../components/shopping_cart/games-in-cart/games'
-import Information from '../components/shopping_cart/information/information'
+//import '../css/style_shopping_cart.css';
+import Games from '../components/shopping-cart/games-in-cart/games'
+import Information from '../components/shopping-cart/information/information'
 import  Header  from "../components/main/header/header";
 import { Footer } from '../components/footer/footer';
 
 const shopping_cart = () => {
   return (
     <>
-    <Header />  
-        <main className="container_my"> 
-            <div className="container">
-                <div className="zagolov">
-                    <p>Ваша корзина</p>
-                </div>  
-                <Information/>
-                <Games/>
-            </div>
-        </main>
+        <Header />  
+        <Information/>
+        <Games/>
         <Footer/>  
 
       </>
