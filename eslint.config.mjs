8f257b0c3f3adb5d091dac827ff4f5eeba0b8215
Terri <polarisedu157@gmail.com>
2024-05-24 00:42:08 +0300

    rules in eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,5 +8,17 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  pluginReactConfig,{
+    rules: {
+      'react/prop-types': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_', caughtErrors: 'none' },
+        ],
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        "@typescript-eslint/no-explicit-any": "off"
+    },
+    ignores: ["src/index.tsx"]
+  }
 ];
