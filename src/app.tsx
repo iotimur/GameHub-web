64d2dbc5e71b65d8dacc0af40f24b06b50437d74
iotimur <timvork03.gmail.com>
2024-05-25 01:06:34 +0300

    Добавил глобальные стили

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { PageRoutes } from "./routes";
+import { globalStyles } from "./global-styles";
+import { Global } from "@emotion/react";
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <PageRoutes/>
+        <Global styles={globalStyles} />
+        <PageRoutes />
       </BrowserRouter>
     </>
   );
