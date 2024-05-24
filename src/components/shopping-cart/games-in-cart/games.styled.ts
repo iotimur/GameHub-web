44cdcd54036a5576fa74ba663a107c25d5c74e24
unknown <nastya.mayorova9@icloud.com>
2024-05-24 23:00:21 +0300

    работа с корзиной настройка для экранов

--- src/components/shopping-cart/games-in-cart/games.styled.ts
+++ src/components/shopping-cart/games-in-cart/games.styled.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled"
 
 export const Oval = styled.div`
-    width: 400px;
+    width: 300px;
     height: 64px;
     border-radius: 20px;
     background-color: #F39A46;
@@ -12,6 +12,18 @@ export const Oval = styled.div`
     color: black;
     font-size: 4vh;
     margin: 40px 300px 30px;
+    @media screen and (max-width: 1920px) {
+        height: 50px;
+        font-size: 3.5vh;
+        max-width: 300px;
+        text-align: center;
+    }
+    @media screen and (max-width: 800px) {
+        height: 45px;
+        font-size: 3vh;
+        max-width: 250px;
+        text-align: center;
+    }
 
 `;
 
@@ -23,62 +35,127 @@ export const Container_my = styled.div`
     margin: 0 auto;
     margin-right:300px;    
     margin-left:300px;
+    @media screen and (max-width: 1200px) {
+        padding: 4vh;
+        margin: 0 20px;
+    }
+    @media screen and (max-width: 800px) {
+        padding: 3vh;
+        margin: 0 10px;
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
-export const H1 = styled.p`
+export const Title1 = styled.p`
     font-style: italic;
     letter-spacing: 10%;
     font-size: 4vh;
     color: white;
     margin: 20px 20px -10px;
+    @media screen and (max-width: 1920px) {
+        font-size: 3.5vh;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 3vh;
+    }
 `;
 
-export const H2 = styled.p`
+export const Title2 = styled.p`
     color: white;
     margin: 0 20px -50px;
     font-weight: normal;
     font-size: 3vh;
+    @media screen and (max-width: 1920px) {
+        font-size: 2.5vh;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 2vh;
+    }
 `;
 
-export const H3 = styled.p`
+export const Title3 = styled.p`
     color: white;
     margin: 80px 20px -50px;
     font-weight: normal;
     font-size: 3vh;
+    @media screen and (max-width: 1920px) {
+        font-size: 2.5vh;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 2vh;
+    }
 `;
 
 export const BigImage = styled.img`
     vertical-align: middle;
     margin:10px;
     margin-right: 20px;
+    @media screen and (max-width: 1920px) {
+        width: 320px;
+    }
+    @media screen and (max-width: 800px) {
+        width: 280px;
+    }
 `;
 
 export const SmallImage = styled.img`
     position: absolute;
-    margin-left: 150px;
-    margin-top: 40px;
-    top:200px;
-    left: 1000px;
+    left: 1000px;    /* Используйте left для горизонтального позиционирования */
+    margin-top: -20px; /* Оставьте margin-top для вертикального позиционирования */
     max-width: 40px;
+
+    @media screen and (max-width: 1920px) {
+        left:58%;  /* Пример изменения позиции для экранов шириной 1920px и меньше */
+        
+    }
+
     @media screen and (max-width: 1200px) {
-  d isplay: none;
-}
-`;
+        left: 77%;  /* Настройка для экранов шириной 1200px и меньше */
+        max-width: 25px;
+        margin-top:15px;
+    }
+
+    @media screen and (max-width: 800px) {
+        left: 40%;  /* Настройка для экранов шириной 800px и меньше */
+        max-width: 25px;
+    }
+    `;
+
 
 export const Price = styled.p`
     font-weight: bold;
     font-size: 3vh;
     color: white;
-    margin: -10px 0 0 1000px; /* Отрицательное значение для margin-left */
+    margin: -10px 0 0 700px; /* Отрицательное значение для margin-left */
+    @media screen and (max-width: 1920px) {
+        margin-left: 600px;
+        text-align: center;
+        font-size: 3vh;
+    }
+    @media screen and (max-width: 1200px) {
+        margin-left: 300px;
+        text-align: center;
+        font-size: 2.5vh;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 200px;
+        text-align: center;
+        font-size: 2vh;
+    }
 `;
 
 
 export const Delete = styled.p`
-    margin: 130px auto 30px 1000px;
+    margin: 130px auto 30px 700px;
     font-size: 2vh;
     font-weight: bold;
     color: red;
     text-decoration: underline;
+    @media screen and (max-width: 1920px) {
+        margin-left: 10vh;
+        text-align: center;
+    }
 `;
 
 export const Hr = styled.hr`
--- src/components/shopping-cart/information/information.styled.ts
+++ src/components/shopping-cart/information/information.styled.ts
@@ -1,20 +1,28 @@
 import styled from "@emotion/styled"
 
 export const Delete1 = styled.p`
-    margin-top: 10px;
     font-size: 2vh;
     font-weight: bold;
     color: red;
     text-decoration: underline;
     position: absolute;
-    right: 250px; /* Правый отступ */
-    top: 1500px; /* Верхний отступ */
-    margin-top: 250px;
-    margin-right: 100px;
-    
+    top: 1500px;
+    margin-top: 330px;
+   
+    @media screen and (max-width: 1920px) {
+        right: 200px;
+        top: 1200px;
+        margin-right: 150px;
+    }
+    @media screen and (max-width: 800px) {
+        right: 150px;
+        top: 1000px;
+        margin-right: 50px;
+        font-size: 1.8vh;
+    }
 `;
 
-export const Oval1 = styled.div` 
+export const Oval1 = styled.div`
     width: 300px;
     height: 60px;
     border-radius: 20px;
@@ -23,14 +31,23 @@ export const Oval1 = styled.div`
     justify-content: center;
     align-items: center;
     font-weight: bold;
-    color: black; 
-    font-size: 2.8vh; 
+    color: black;
+    font-size: 2.8vh;
     margin-left: auto;
-    margin-right: 400px;
-    margin-bottom:20px;
+    margin-right: 250px;
+    margin-bottom: 20px;
+
+    @media screen and (max-width: 1200px) {
+        margin-right: 300px;
+        margin-top: 10px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-right: 200px;
+        font-size: 2.5vh;
+    }
 `;
 
-export const Oval2 = styled.div` 
+export const Oval2 = styled.div`
     width: 300px;
     height: 60px;
     border-radius: 20px;
@@ -39,34 +56,65 @@ export const Oval2 = styled.div`
     justify-content: center;
     align-items: center;
     font-weight: bold;
-    color: black; 
-    font-size: 2.8vh; 
-    margin-left: 850px;
+    color: black;
+    font-size: 2.8vh;
+    margin-left: 700px;
     margin-right: auto;
-    margin-top:-100px;
+    margin-top: -80px;
     margin-bottom: 70px;
-`;
 
+    @media screen and (max-width: 1200px) {
+        margin-top:10px;
+        margin-left: 190px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 450px;
+        font-size: 2.5vh;
+    }
+`;
 
 export const Price1 = styled.div`
-    font-weight: bold; /* Задаем полужирное начертание */
-    font-size: 3vh; 
+    font-weight: bold;
+    font-size: 3vh;
     color: white;
-    margin-top: 0px;
-    margin-right:100px;
-`; 
+    margin-top: 5px;
+    margin-right: 250px;
 
-export const Total = styled.div`    
+    @media screen and (max-width: 1200px) {
+        margin-top:5px;
+        margin-right: 200px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-right: 150px;
+        font-size: 2.5vh;
+    }
+`;
+
+export const Total = styled.div`
     color: white;
     display: flex;
-    justify-content: space-around; /* Центрирование элементов */
+    justify-content: space-around;
     margin-top: 150px;
     font-weight: bold;
     font-size: 30px;
+
+    @media screen and (max-width: 1200px) {
+        font-size: 26px;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 22px;
+    }
 `;
 
 export const TotalSpan = styled.span`
     margin-left: 550px;
+
+    @media screen and (max-width: 1200px) {
+        margin-left: 200px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 200px;
+    }
 `;
 
 export const Info = styled.div`
@@ -75,30 +123,56 @@ export const Info = styled.div`
     grid-template-columns: auto;
     font-size: 3vh;
     margin-left: 200px;
+
+    @media screen and (max-width: 1200px) {
+        margin-left: 150px;
+        font-size: 2.5vh;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 100px;
+        font-size: 2vh;
+    }
 `;
 
 export const H4 = styled.p`
     margin-top: 30px;
-    margin-left: 200px;
+    margin-left: 50px;
     color: white;
     margin-right: 10px;
     display: inline;
     text-decoration: underline;
+
+    @media screen and (max-width: 120px) {
+        margin-left: 150px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 100px;
+        font-size: 2.5vh;
+    }
 `;
 
 export const NDS = styled.p`
     display: inline;
     color: white;
     font-style: italic;
-    margin-left:200px;
-    margin-bottom:-1000px;
-    margin-left: 350px;
-`;
+    margin-left: 250px;
 
+    @media screen and (max-width: 1200px) {
+        margin-left: 200px;
+    }
+    @media screen and (max-width: 800px) {
+        margin-left: 100px;
+        font-size: 2.5vh;
+    }
+`;
 
 export const Hr = styled.hr`
     width: 100%;
     border: 0;
-    border-top: 1px solid #ccc; /* Пример стилей линии */
-    margin: 20px 0; /* Пример отступов */
+    border-top: 1px solid #ccc;
+    margin: 20px 0;
+
+    @media screen and (max-width: 800px) {
+        margin: 15px 0;
+    }
 `;
