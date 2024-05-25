70ae8124cfc6718c2cef143ce976332cd68f367c
aaeii <ahm-emily@yandex.ru>
2024-05-25 16:14:21 +0300

    stubs   categories

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,3 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+router.get("/game-page", (request, response) => {
+  response.send(require("../json/gamepage/success.json"));
+});
+
+router.get("/categories", (request, response) => {
+    response.send(require("../json/gamepage/success.json"));
+  });
 
 module.exports = router;
