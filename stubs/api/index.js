de6346bba19518f3b7875654e6597f46812602ee
unknown <nastya.mayorova9@icloud.com>
2024-05-25 15:42:26 +0300

    работа корзины с json

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -4,4 +4,8 @@ router.get("/game-page", (request, response) => {
   response.send(require("../json/gamepage/success.json"));
 });
 
+router.get("/shopping-cart", (request, response) => {
+  response.send(require("../json/shopping-cart/success.json"));
+});
+
 module.exports = router;
