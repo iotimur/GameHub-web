bc2555d8f01afe9aeb0fd8578e74222b17fb5b9c
TIMUR MUKHAMETOV <timvork03@gmail.com>
2024-12-12 10:44:05 +0300

    getNavigationsValue -> getNavigationValue

--- src/_data_/urls.ts
+++ src/_data_/urls.ts
@@ -1,19 +1,30 @@
-import { getNavigationsValue, getConfigValue } from '@brojs/cli';
+import { getNavigationValue, getConfigValue } from "@brojs/cli";
 
-
-const baseUrl = getNavigationsValue('gamehub.main');
+const baseUrl = getNavigationValue("gamehub.main");
 
 export const URLs = {
   baseUrl,
   ui: {
-    categories: getNavigationsValue('gamehub.categories') && `${baseUrl}${getNavigationsValue('gamehub.categories')}`,
-    entrance: getNavigationsValue('gamehub.entrance') && `${baseUrl}${getNavigationsValue('gamehub.entrance')}`,
-    registration: getNavigationsValue('gamehub.registration') && `${baseUrl}${getNavigationsValue('gamehub.registration')}`,
-    'personal-account': getNavigationsValue('gamehub.personal-account') && `${baseUrl}${getNavigationsValue('gamehub.personal-account')}`,
-    'shopping-cart': getNavigationsValue('gamehub.shopping-cart') && `${baseUrl}${getNavigationsValue('gamehub.shopping-cart')}`,
-    'game-page': getNavigationsValue('gamehub.game-page') && `${baseUrl}${getNavigationsValue('gamehub.game-page')}`,
+    categories:
+      getNavigationValue("gamehub.categories") &&
+      `${baseUrl}${getNavigationValue("gamehub.categories")}`,
+    entrance:
+      getNavigationValue("gamehub.entrance") &&
+      `${baseUrl}${getNavigationValue("gamehub.entrance")}`,
+    registration:
+      getNavigationValue("gamehub.registration") &&
+      `${baseUrl}${getNavigationValue("gamehub.registration")}`,
+    "personal-account":
+      getNavigationValue("gamehub.personal-account") &&
+      `${baseUrl}${getNavigationValue("gamehub.personal-account")}`,
+    "shopping-cart":
+      getNavigationValue("gamehub.shopping-cart") &&
+      `${baseUrl}${getNavigationValue("gamehub.shopping-cart")}`,
+    "game-page":
+      getNavigationValue("gamehub.game-page") &&
+      `${baseUrl}${getNavigationValue("gamehub.game-page")}`,
   },
   api: {
-    main: getConfigValue('gamehub.api')
+    main: getConfigValue("gamehub.api"),
   },
-}
+};
