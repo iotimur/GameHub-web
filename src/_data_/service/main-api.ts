4588ba4103fa0fc6340ac1ddd953d5d42ce866c6
unknown <nastya.mayorova9@icloud.com>
2024-12-20 15:57:11 +0300

    api для корзины

--- src/_data_/service/main-api.ts
+++ src/_data_/service/main-api.ts
@@ -1,7 +1,7 @@
-// main-api.ts
 import { getConfigValue } from "@brojs/cli";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Home, BaseResponse } from "../model/common_home";
+import { GamesResponse } from "../model/common_games"; // Импорт модели для нового эндпоинта
 
 const baseUrl = getConfigValue("gamehub.api");
 
@@ -10,16 +10,33 @@ export const mainApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }), // Настройка базового URL
   tagTypes: ["App"], // Метки для кэширования
   endpoints: (builder) => ({
-    // Запрос для получения данных
+    // Эндпоинт для главной страницы
     homePage: builder.query<Home, void>({
-      query: () => "/home", // Эндпоинт для получения данных
+      query: () => "/home", // URL для получения данных
       transformResponse: (response: BaseResponse<Home>) => {
         if (response.success === true) {
-          return response?.data || { topSail: [], categories: [], news: [] }; // Возвращаем корректную структуру по умолчанию
+          return response?.data || { topSail: [], categories: [], news: [] }; // Структура по умолчанию
         } else {
           return { topSail: [], categories: [], news: [] }; // Пустая структура в случае ошибки
         }
       },
     }),
+
+    // Эндпоинт для корзины
+    gamesInCart: builder.query<GamesResponse, void>({
+      query: () => "/shopping-cart/success", // URL для получения данных
+      transformResponse: (response: BaseResponse<GamesResponse>): GamesResponse => {
+        if (response.success === true) {
+          return response?.data || { GamesResponse: [] }; // Структура по умолчанию
+        } else {
+          return { Game: [], GamesResponse: [] }; // Пустая структура в случае ошибки
+        }
+      },
+    }),
   }),
 });
+
+// Экспортируем хуки для использования в компонентах
+export const { useHomePageQuery, useGamesInCartQuery } = mainApi;
+
+export default mainApi;
