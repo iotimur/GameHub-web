8293f9945e65ea1df11a34595c3b1cb1b25b1a22
TIMUR MUKHAMETOV <timvork03@gmail.com>
2025-02-08 12:05:39 +0300

    добавлять в корзину в game-page

--- src/components/game-page/price-block/price-block.tsx
+++ src/components/game-page/price-block/price-block.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import * as getCartGamesSelectors from "../../../_data_/selectors/cart-games"; // Подключаем селектор корзины
+import { useAddToCartMutation } from "../../../_data_/service/main-api";
+import { useDispatch, useSelector } from "react-redux";
+import { cartSlice } from "../../../_data_/slices/cart-games";
 
 import {
     PriceBlockContainer,
@@ -16,6 +20,33 @@ import {
 
 const PriceBlock: React.FC = () => {
     const { t } = useTranslation();
+    const cartIds = useSelector(getCartGamesSelectors.ids); // Получаем список id игр в корзине
+    const [isUpdating, setIsUpdating] = useState(false);
+    const [modifyCart] = useAddToCartMutation();
+    const dispatch = useDispatch();
+
+    const gameId = 1; // ID игры, которую проверяем
+    const isInCart = cartIds?.includes(gameId);
+
+    const handleCartUpdate = async () => {
+        const action = isInCart ? "remove" : "add";
+        setIsUpdating(true);
+
+        try {
+            await modifyCart({ id: gameId, action }).unwrap();
+
+            // Локальное обновление store
+            if (isInCart) {
+                dispatch(cartSlice.actions.removeFromCart(gameId));
+            } else {
+                dispatch(cartSlice.actions.addToCart(gameId));
+            }
+        } catch (error) {
+            console.error("Ошибка при обновлении корзины:", error);
+        } finally {
+            setIsUpdating(false);
+        }
+    };
 
     return (
         <PriceBlockContainer>
@@ -29,13 +60,14 @@ const PriceBlock: React.FC = () => {
             <PriceImage>
                 {/* Изображение логотипа */}
             </PriceImage>
-            <Link to="/gamehub/shopping-cart">
-                <PriceRectangle>
-                    <PriceText>{t('price.addToCart')}</PriceText>
-                </PriceRectangle>
-            </Link>
+            <PriceRectangle 
+                style={{ backgroundColor: isInCart ? "#4CAF50" : "#f39a46" }} 
+                onClick={handleCartUpdate}
+            >
+                <PriceText>{isInCart ? "В корзине" : t('price.addToCart')}</PriceText>
+            </PriceRectangle>
         </PriceBlockContainer>
     );
-}
+};
 
 export default PriceBlock;
